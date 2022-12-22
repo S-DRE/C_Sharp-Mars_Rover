8776f3deb921924f3618db1ac36db5669d8d5241
Irene PR <ipice1618@protonmail.com>
2022-12-22 13:08:05 +0100

    Mars rover rotates to the left

--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -4,6 +4,15 @@ public class Rover
 {
     public string Execute(string command)
     {
-        return "0:0:N";
+        var direction = "N";
+        
+        foreach (var instruction in command)
+        {
+            if (direction.Equals("N")) direction = "W";
+            else if (direction.Equals("W")) direction = "S";
+            else if (direction.Equals("S")) direction = "E";
+            else if (direction.Equals("E")) direction = "N";
+        }
+        return $"0:0:{direction}";
     }
 }
--- MarsRoverTest/RoverTest.cs
+++ MarsRoverTest/RoverTest.cs
@@ -4,13 +4,33 @@ namespace MarsRoverTest;
 
 public class RoverTest
 {
+    private Rover rover;
+
+    public RoverTest()
+    {
+        this.rover = new Rover();
+    }
+
+    [Theory] // test with multiple data - parametrized
+    [InlineData("L", "0:0:W")]
+    [InlineData("LL", "0:0:S")]
+    [InlineData("LLL", "0:0:E")]
+    [InlineData("LLLL", "0:0:N")]
+    public void TheRoverRotatesToTheLeft(string command, string expectedResult)
+    {
+        var startingPosition = rover.Execute(command);
+
+        Assert.Equal(expectedResult, startingPosition);
+    }
+    
+
     [Fact] // <-- Single test
     public void ThatStartsIn00N()
     {
-        Rover rover = new Rover();
+        var startingPosition = rover.Execute("");
 
-        String startingPosition = rover.Execute("");
-        
-        Assert.Equal("0:0:N", startingPosition);
+        var expectedStartingPosition = "0:0:N";
+        Assert.Equal(expectedStartingPosition, startingPosition);
     }
+
 }
