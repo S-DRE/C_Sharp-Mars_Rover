95ac778f0610b98ab13ef86b401fb1fbfc61d907
S-DRE <sdreworks@gmail.com>
2023-04-12 17:33:43 +0200

    Simple refactoring

--- MarsRoverTest/RoverTest.cs
+++ MarsRoverTest/RoverTest.cs
@@ -4,14 +4,14 @@ namespace MarsRoverTest;
 
 public class RoverTest
 {
-    private Rover rover;
+    private readonly Rover rover;
 
     public RoverTest()
     {
         rover = new Rover();
     }
 
-    [Theory] // Test with multiple data - Parametrized
+    [Theory] // <-- Test with multiple data - Parameterized
     [InlineData("L", "0:0:W")]
     [InlineData("LL", "0:0:S")]
     [InlineData("LLL", "0:0:E")]
@@ -33,7 +33,7 @@ public class RoverTest
         Assert.Equal(expectedStartingPosition, startingPosition);
     }
     
-    [Theory] // Test with multiple data - Parametrized
+    [Theory] // <-- Test with multiple data - Parameterized
     [InlineData("R", "0:0:E")]
     [InlineData("RR", "0:0:S")]
     [InlineData("RRR", "0:0:W")]
